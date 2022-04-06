e6f0712d62a50fbf4b8ee41451c573810b0d6f9f
RobertKS123 <68371027+RobertKS123@users.noreply.github.com>
2022-04-06 13:02:37 +0200

    Changes to navbar

---
+++ src/pages/account/login/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Login() {
+  return (
+      <div className='login'>
+          <h1>
+              this is where the Login page goes
+          </h1>
+      </div>
+  )
+}
+
+export default Login
