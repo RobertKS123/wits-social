0c37f94b4160139815d1e1c155021a69324c483c
RobertKS123 <68371027+RobertKS123@users.noreply.github.com>
2022-04-06 16:28:24 +0200

    Signup added with some funtionality

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import Navbar from './components/navbar/Navbar';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './pages/home/Home';
-import Login from './pages/account/login/Login';
+import Login from './components/login/Signup';
 import Account from './pages/account/manage/ManageAccount';
-
 
 function App() {
   return (
@@ -13,7 +12,7 @@ function App() {
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home}/>
-          <Route path='/login' exact component={Login}/>
+          {<Route path='/login' exact  component={Login}/>}
           <Route path='/account' exact component={Account}/>
         </Switch>
       </Router>
---
+++ src/components/login/Signup.js
@@ -0,0 +1,93 @@
+import React, {useState} from 'react'
+import Validation from './Validation'; 
+
+const Signup = () => {
+
+    const [values, setVlaues] = useState({
+        username: '',
+        email: '',
+        password: '',
+    })
+
+    const [success,setSuccess] = useState(false);
+
+    const [errors, setErrors] = useState({});
+
+    const handleChange = (event) => {
+        setVlaues({
+            ...values,
+            [event.target.name]: event.target.value,
+        });
+    }
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        setErrors(Validation(values));
+        setSuccess(true);
+    }
+
+
+    return (
+        <>
+            {success ? (
+                <div>
+                    <h1>Account Created!</h1>
+                    <br />
+                    <p>
+                        {/* This is where the user will be directed after signup */}
+                        <a href='#'>Continue to Wits-Social</a> 
+                    </p>
+                </div>
+            ) : (
+            <div className='container'>
+                <div className='app-wrapper'>
+                    <h2 className='title'>Create Account</h2>
+                </div>
+                <form className='form-wrapper'>
+                    <div className='name'>
+                        <label className='lable'>UserName</label>
+                        <input 
+                            className='input' 
+                            type='text' 
+                            name='username' 
+                            value={values.username}
+                            onChange={handleChange}
+                            required
+                        />
+                        {errors.username && <p className='errormessage'>{errors.username}</p>}
+                    </div>
+                    <div className='name'>
+                        <label className='lable'>Email Adress</label>
+                        <input 
+                            className='input' 
+                            type='email' 
+                            name='email'
+                            value={values.email}
+                            onChange={handleChange}
+                            required
+                        />
+                        {errors.email && <p className='errormessage'>{errors.email}</p>}
+                    </div>
+                    <div className='name'>
+                        <label className='lable'>Password</label>
+                        <input 
+                            className='input' 
+                            type='password' 
+                            name='password'
+                            value={values.password}
+                            onChange={handleChange}
+                            required
+                        />
+                        {errors.password && <p className='errormessage'>{errors.password}</p>}
+                    </div>
+                    <div>
+                        <button className='submit' onClick={handleFormSubmit}>Signup</button>
+                    </div>
+                </form>
+            </div>
+            )}
+        </>
+    )
+}
+
+export default Signup
