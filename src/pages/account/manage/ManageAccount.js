72f5cafbe8510f123b81887c8d0a020656a774e7
RobertKS123 <68371027+RobertKS123@users.noreply.github.com>
2022-04-06 13:01:59 +0200

    Added a navigation bar

---
+++ src/pages/account/manage/ManageAccount.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Account() {
+    return (
+        <div className='manage'>
+            <h1>
+                this is where the Account Manage page goes
+            </h1>
+        </div>
+    )
+}
+
+export default Account
