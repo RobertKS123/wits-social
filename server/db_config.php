ec463b512b52af980f8a9e883798afcc4a8e03fb
Alan-Anthony-Burke <thealanguy@gmail.com>
2022-04-29 15:07:37 +0200

    Minor changes

--- server/db_config.php
+++ server/db_config.php
@@ -2,9 +2,10 @@
 
 //This file must be required_once in all php files that need to access the database
 
-$username = "id18696689_startechies45907de34kh28";
-$password = "f_ia5F3<fZ*[}LV>";
-$database = "id18696689_startechies2347vgh3";
+//Details hidden because of pub;ic github repo
+$username = "hidden";
+$password = "hidden";
+$database = "hidden";
 
 $link = mysqli_connect("localhost", $username, $password, $database);
 if ($link->connect_error) {
