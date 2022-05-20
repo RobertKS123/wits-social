75cb93b58a3e2ba711338170d0592df9c76baa5c
RobertKS123 <68371027+RobertKS123@users.noreply.github.com>
2022-05-20 14:46:40 +0200

    upload page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Signup from './components/login/Signup';
 import Account from './pages/account/manage/ManageAccount';
 import Login from './components/login/Login';
 import BottomNavbar from './components/bottomNavbar/BottomNavbar';
-//import LandingPage from './pages/landing_page/LandingPage';
+import Upload from './components/upload/Uplaod';
 import TopForYou from './pages/home/TopForYou/TopForYou';
 import LandingPage from './pages/landingPage/LandingPage';
 
@@ -16,14 +16,15 @@ function App() {
 return (
 	<>  		
 		<Router>
+		<Route path='/' exact  component={Upload}/>
 		<BottomNavbar />
-		<Route path='/' exact component={LandingPage} />
+		{/* <Route path='/' exact component={LandingPage} /> */}
 		<Route path='/signup' exact  component={Signup}/>
 		<Route path='/login' exact  component={Login}/>
 		<Route path='/home' exact component={Home}/>
-		{/* <Route path='/trending' exact component={Treanding}/>
-		<Route path='/create' exact  component={Create}/>
-		<Route path ='/inbox' exact component={Inbox}/> */}
+		{/* <Route path='/trending' exact component={Treanding}/>*/}
+		{/*<Route path='/create' exact  component={Upload}/>*/}
+		{/*<Route path ='/inbox' exact component={Inbox}/> */}
 		<Route path='/account' exact component={Account}/>
 		</Router>     
 	</>
---
+++ src/components/upload/Uplaod.js
@@ -0,0 +1,136 @@
+import React,{useState,useEffect,useRef,useContext} from 'react';
+import axios from '../../api/axios';
+import { AuthContext } from '../../api/AuthProvider';
+import { Link,Redirect } from 'react-router-dom';
+import styled from 'styled-components';
+import * as AiIcons from 'react-icons/ai';
+
+const NAME_REGEX =/^/;
+const DESC_REGEX =/^/;
+const UPLOAD_URL = '/';
+
+const Upload = () => {
+    const [state] = useContext(AuthContext);
+
+    const errRef = useRef();
+    const nameRef = useRef();
+
+    const [name, setName] = useState('');
+    const [validName, setValidName] = useState(false);
+    const [nameFocus, setNameFocus] = useState(false);
+
+    const [desc, setDesc] = useState('');
+    const [validDesc, setValidDesc] = useState(false);
+    const [descFocus, setDescFocus] = useState(false);
+
+    const [podcast, setPodcast] = useState('');
+    const [validPodcast, setValidPodcast] = useState(false);
+
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        nameRef.current.focus();
+    }, []);
+
+    useEffect(() => {
+        setValidName(NAME_REGEX.test(name));
+    }, [name]);
+
+    useEffect(() => {
+        setValidDesc(DESC_REGEX.test(desc));
+    }, [desc]);
+
+    useEffect(() => {
+        setValidPodcast(true);
+    }, [podcast]);
+
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+        const v1 = NAME_REGEX.test(name);
+        const v2 = DESC_REGEX.test(desc);
+
+        if (!v1 || !v2 ) {
+            setErrMsg("Invalid Entry");
+            return;
+        }
+
+        //Axios goes here 
+        try {
+            const id = state.id;
+
+            const response = await axios.get(UPLOAD_URL,{params:{id : id, name: name, decription : desc, podcast : podcast }});
+            
+            if (response?.data?.podcastId === 301) { //name taken
+                setErrMsg('This podcast name is already in use');           
+            } else if (response?.data?.audioType === 301) { //not accepted audio type
+                setErrMsg('This is not an accepted ausio type');
+            } else {              
+                setSuccess(true);
+            }
+
+        } catch (err) {
+            if (!err?.response) {
+                setErrMsg('No Server Response');
+            } else {
+                setErrMsg('Upload Failed')
+            }
+            errRef.current.focus();
+        }
+    }
+
+    return(
+        <section className='w3-monospace'>
+            <h1>Create!</h1>
+            <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"}>{errMsg}</p>
+            <form onSubmit={handleSubmit} encType='multipart/form-data'>
+                <div className='input-container'>
+                    {/*<BsIcons.BsFillPersonFill className='input-icon'/>*/}
+                    <input
+                        type="text"
+                        id="name"
+                        placeholder='Podcast Name'
+                        ref={nameRef}
+                        autoComplete="off"
+                        onChange={(e) => setName(e.target.value)}
+                        value={name}
+                        required
+                        onFocus={() => setNameFocus(true)}
+                        onBlur={() => setNameFocus(false)}
+                    />
+                    {/* <FontAwesomeIcon icon={faCheck} className={validName ? "valid" : "hide"} />
+                    <FontAwesomeIcon icon={faTimes} className={validName || !user ? "hide" : "invalid"} /> */}
+                </div>
+                <div className='input-container'>
+                    {/*<BsIcons.BsFillPersonFill className='input-icon'/>*/}
+                    <textarea
+                        type="text"
+                        id="desc"
+                        placeholder='Podcast Description'
+                        onChange={(e) => setDesc(e.target.value)}
+                        value={desc}
+                        required
+                        onFocus={() => setDescFocus(true)}
+                        onBlur={() => setDescFocus(false)}
+                    />
+                    {/* <FontAwesomeIcon icon={faCheck} className={validName ? "valid" : "hide"} />
+                    <FontAwesomeIcon icon={faTimes} className={validName || !user ? "hide" : "invalid"} /> */}
+                </div>
+                <div className='input-container'>
+                    <p>Add your podcast here</p>
+                    <AiIcons.AiOutlinePlusCircle />
+                    <input
+                        type='file'
+                        id="podcast"
+                        accept='audio/mp3, audio/wav'
+                        onChange={(e) => setPodcast(e.target.value)}
+                        // style={{display: 'none'}}
+                    />
+                </div>
+                <button disabled={!validName || !validDesc ? true : false}>Upload</button>
+            </form>
+        </section>
+    );
+}
+
+export default Upload
