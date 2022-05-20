61ca8ea3ae21df038ee1985cfc888d9b4ed92789
RobertKS123 <68371027+RobertKS123@users.noreply.github.com>
2022-05-20 15:34:49 +0200

    likes changes

--- src/pages/Podcast/Podcast.js
+++ src/pages/Podcast/Podcast.js
@@ -2,33 +2,37 @@ import React, { useState, useEffect, useRef, location, parent, useContext, } fro
 import './Podcast.css';
 import AudioComponent from "./AudioComponent";
 import CommentComponent from "./CommentComponent";
+import * as AiIcons from 'react-icons/ai';
 import LikeComponent from "./LikeComponent";
 import { AuthContext } from "../../api/AuthProvider";
 import axios from '../../api/axios';
 
-const REGISTER_URL = '/podcast_scripts/like_unlike_podcast.php'; //link to database
+const LIKES_URL = '/podcast_scripts/like_unlike_podcast.php'; //link to database
 
 function Podcast(props) {
+  const [state] = useContext(AuthContext);
   
     const [success, setSuccess] = useState(false); //maybe button changes colour - in css
     const [errMsg, setErrMsg] = useState('');
 
     const [likes, setLikes] = useState(props.podcast_likes);
-    const [likeState, setLikesStates] = useState(false);
-
-    useEffect(() => {
-        setErrMsg('');
-    }, [])
+    const [likeState, setLikesState] = useState(false);
 
     const handleSubmit = async (e) => { //wehn someone presses submit on the form
         e.preventDefault();
         // prevents button hack
-
         //Axios - speaks to ther server
         try {                                                     //database_variable ; my_variable
-            const response = await axios.get(REGISTER_URL,{params:{ podcast_id: props.podcast_id, username: state.id}});
+            const response = await axios.get(LIKES_URL,{params:{ podcast_id: props.podcast_id, username: state.id}});
             setSuccess(true);
-            setLikes(response?.data?.podcast_likes);
+            setLikes(response?.data?.podcast_likes); //Change based on php response
+            if (response?.data?.likestate === 1){
+              setLikesState(true);
+            }
+            if (response?.data?.likeState === 0){
+              setLikesState(false);
+            }
+            //setLikesState(true);
         } catch (err) {
             if (!err?.response) {
                 setErrMsg('No Server Response');
@@ -39,13 +43,6 @@ function Podcast(props) {
 
         console.log("NUMBER OF LIKES = ", likes);
     }
-  
-  // function handleClick(e){ 
-  //   e.preventDefault();    
-  //   console.log('The link was clicked.');  
-  // }
-  
-  const [state] = useContext(AuthContext);
 
   //const url = "https://startechies.000webhostapp.com/end_users/banners/banner_sample_1.png";    
     return(
@@ -58,21 +55,15 @@ function Podcast(props) {
             <br/>
             <div className="podcastDescription"> {props.podcast_description}</div>
             <div className="podcastCreator"><span className="badge badge-dark">Uploaded by {props.podcast_username}</span></div>
-          </div>
-          
-          {/* LIKE BUTTON: 
-          https://startechies.000webhostapp.com/server/podcast_scripts/like_unlike_podcast.php'+'?user_id='+props.podcast_user_id+'?podcast_id='+props.podcast_id*/}
-          {/* <a href="#" onClick={handleClick}> LIKES = {props.podcast_likes}</a> */}
-          {/* <a href="#" onClick={location.href='https://startechies.000webhostapp.com/server/podcast_scripts/like_unlike_podcast.php'+'?user_id='+props.podcast_user_id+'?podcast_id='+props.podcast_id} > LIKES = {props.podcast_likes}</a> */}
-          {/* <a href={"https://startechies.000webhostapp.com/server/podcast_scripts/like_unlike_podcast.php?podcast_id="+props.podcast_id+"?user_id="+state.id}> THIS ONE = {props.podcast_likes}</a> */}
-          
-          <LikeComponent podcast_id_1={props.podcast_id} podcast_likes_1={props.podcast_likes} />
-          <form onSubmit={handleSubmit}>
-            <button>Sign Up = {likes}</button>
-          </form>
+          </div>                   
           <br/>
-        <AudioComponent audio_url={props.podcast_audio}/>
-        <br/> 
+          <AudioComponent audio_url={props.podcast_audio}/>
+          <button className={likeState ? 'likes active' : 'likes'} onClick={handleSubmit}>
+            <div>
+              <AiIcons.AiTwotoneLike/>
+              {likes}
+            </div>
+          </button>
         <br/> 
         <CommentComponent podcast_id_comment = {props.podcast_id}/>
       </div>
