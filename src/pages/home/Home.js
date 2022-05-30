8f715e7538b7eb8da5c730930268a9d8f18bc3ea
Alan-Anthony-Burke <thealanguy@gmail.com>
2022-05-30 16:25:52 +0200

    Navbar and snap scrolling working properly on home screen

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -21,6 +21,13 @@ function Home() {
       return result;
     }
 
+    function setInnerHeightOnMobile(){
+      // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+      let vh = window.innerHeight * 0.01;
+      // Then we set the value in the --vh custom property to the root of the document
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+
     let json_1;
     const [users, setUsers] = useState([]);
 
@@ -91,6 +98,7 @@ function Home() {
 
         <>
           <div className="outer-box">
+            <script>{setInnerHeightOnMobile()}</script>
             {/* <div className="inner-podcast-item"></div> */}
             {createPodcastSquares_2(array_json)}
           </div>
