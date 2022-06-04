488b1bfea0cc55e29968e7639a83ad7e84aeccd4
RobertKS123 <68371027+RobertKS123@users.noreply.github.com>
2022-06-04 17:14:14 +0200

    podcast layout

--- src/pages/Podcast/Podcast.js
+++ src/pages/Podcast/Podcast.js
@@ -23,7 +23,6 @@ function Podcast(props) {
 
     const handleSubmit = async (e) => { //wehn someone presses the like button
         e.preventDefault();
-
         //Axios - speaks to ther server
         try {                                                     //database_variable ; my_variable
             const response = await axios.get(LIKES_URL,{params:{ podcast_id: props.podcast_id, user_id: state.id}});
@@ -36,12 +35,12 @@ function Podcast(props) {
             // console.log("USER_ID = ", state.id);
 
             if (response?.data?.like_response === true){
-              setLikesState(true);
-              //setLikes(props.podcast_likes-1)
+                setLikesState(true);
+                //setLikes(props.podcast_likes-1)
             }
             if (response?.data?.like_response === false){
-              setLikesState(false);
-              //setLikes(props.podcast_likes+1)
+                setLikesState(false);
+                //setLikes(props.podcast_likes+1)
             }
         } catch (err) {
             if (!err?.response) {
@@ -54,117 +53,121 @@ function Podcast(props) {
     let json_2;
 
     useEffect(() => {
-      
-      const response_2 = axios.get(LIKED_BEFORE_URL,{params:{ podcast_id: props.podcast_id, user_id: state.id}});
-      var final_response;
-      
-      response_2.then(function(result){
-        final_response = result.data;
-        if (final_response === "liked"){
-          setLikesState(true);
+        const response_2 = axios.get(LIKED_BEFORE_URL,{params:{ podcast_id: props.podcast_id, user_id: state.id}});
+        var final_response;
+
+        response_2.then(function(result){
+            final_response = result.data;
+            if (final_response === "liked"){
+                setLikesState(true);
+            }
+            if (final_response === "not_liked"){ //response_2?.data?.like_response
+                setLikesState(false);
+            }
         }
-        if (final_response === "not_liked"){ //response_2?.data?.like_response
-          setLikesState(false);
-        }
-      })
-    }, []);
+    )}, []);
 
 
-  //const url = "https://startechies.000webhostapp.com/end_users/banners/banner_sample_1.png";    
-  
-  if (likeState === true){ //this means liked
-    return(
-      
-      <div className="podcastOuter bg-dark" style={ { backgroundImage: `url(${props.podcast_back_image})`}} >
-        <img src={props.podcast_profile_image} className="profilePic img-thumbnail mx-auto d-block"/>
-        
-          <div className="podcastTitle">
-          <span className="badge badge-pill badge-light podcastTitle">{props.podcast_title}</span>
-          </div>
-          {/* <span className="badge badge-dark">{props.podcast_title}</span>  */}
-            <br/>
-            <div className="podcastDescription"><span class="badge badge-primary">{props.podcast_description}</span></div>
-            <div className="podcastCreator"><span className="badge badge-dark">Uploaded by {props.podcast_username}</span></div>
-          
-          <AudioComponent audio_url={props.podcast_audio}/>
-          
-          <button className={likeState ? 'likes active' : 'likes'} onClick={handleSubmit}>
-            <div>
-              <FcIcons.FcLike  className='input-icon' size={25}/>
-              <span style={{fontSize: 28}}>{likes}</span>
+    if (likeState === true){ //this means liked
+        return(
+            <div className="podcastOuter bg-dark" style={ { backgroundImage: `url(${props.podcast_back_image})`}} >
+                <img src={props.podcast_profile_image} className="profilePic img-thumbnail mx-auto d-block"/>
+            
+                <div className="podcastTitle">
+                    <span className="badge badge-pill badge-light podcastTitle">{props.podcast_title}</span>
+                </div>
+                <div className="podcastDescription">
+                    <span class="badge badge-primary">
+                        {props.podcast_description}
+                    </span>
+                </div>
+                <div className="podcastCreator">
+                    <span className="badge badge-dark">
+                        Uploaded by {props.podcast_username}
+                    </span>
+                </div>
+                <AudioComponent audio_url={props.podcast_audio}/>
+                <button className={likeState ? 'likes active' : 'likes'} onClick={handleSubmit}>
+                    <div>
+                        <FcIcons.FcLike  className='input-icon' size={25}/>
+                        <span style={{fontSize: 28}}>
+                            {likes}
+                        </span>
+                    </div>
+                </button>
+                <CommentComponent podcast_id_comment = {props.podcast_id}/>
             </div>
-          </button>
-        <CommentComponent podcast_id_comment = {props.podcast_id}/>
-      </div>
+        )
+    } else { //this means unliked
+    return(
+        <div className="podcastOuter bg-dark" style={ { backgroundImage: `url(${props.podcast_back_image})`}} >
+            <img src={props.podcast_profile_image} className="profilePic img-thumbnail mx-auto d-block"/>
+            <div className="podcastTitle">
+                <span className="badge badge-pill badge-light podcastTitle">
+                    {props.podcast_title}
+                </span>
+            </div>
+            <div className="podcastDescription">
+                <span class="badge badge-primary">
+                    {props.podcast_description}
+                </span>
+            </div>
+            <div className="podcastCreator">
+                <span className="badge badge-dark">
+                    Uploaded by {props.podcast_username}
+                </span>
+            </div>          
+            <AudioComponent audio_url={props.podcast_audio}/>
+            <button className={likeState ? 'likes active' : 'likes'} onClick={handleSubmit}>
+                <div>
+                    <AiIcons.AiOutlineLike className='input-icon' size={25} color={"white"}/> 
+                    <span style={{fontSize: 28}}>
+                        {likes}
+                    </span>
+                </div>
+            </button>
+            <CommentComponent podcast_id_comment = {props.podcast_id}/>
+        </div>
     )
     }
-  
-  else { //this means unliked
-    return(
-      
-      <div className="podcastOuter bg-dark" style={ { backgroundImage: `url(${props.podcast_back_image})`}} >
-        <img src={props.podcast_profile_image} className="profilePic img-thumbnail mx-auto d-block"/>
-        
-        <div className="podcastTitle">
-        {/* <span>{props.podcast_title}</span> */}
-        <span className="badge badge-pill badge-light podcastTitle">{props.podcast_title}</span>
-          </div>
-          
-          {/* <span className="badge badge-dark">{props.podcast_title}</span>  */}
-            <br/>
-            <div className="podcastDescription"><span class="badge badge-primary">{props.podcast_description}</span></div>
-            <div className="podcastCreator"><span className="badge badge-dark">Uploaded by {props.podcast_username}</span></div>          
-          <AudioComponent audio_url={props.podcast_audio}/>
-          
-          <button className={likeState ? 'likes active' : 'likes'} onClick={handleSubmit}>
-            <div>
-              <AiIcons.AiOutlineLike className='input-icon' size={25} color={"white"}/> 
-              <span style={{fontSize: 28}}>{likes}</span>
-            </div>
-          </button>
-        <CommentComponent podcast_id_comment = {props.podcast_id}/>
-      </div>
-    )
-    }
-  }
-  
+}
 
-  export default Podcast
+export default Podcast
 
-  {/*
+    {/*
 
 <div className="podcastOuter bg-dark" style={{ 
       //backgroundImage: `url("https://startechies.000webhostapp.com/end_users/banners/banner_sample_1.png")` 
-      backgroundImage: `url(${props.podcast_back_image})`
+        backgroundImage: `url(${props.podcast_back_image})`
     }}>
         <img src={props.podcast_profile_image} className="profilePic img-thumbnail mx-auto d-block"/>
         <br/>
-          <div className="podcastTitle text-white">
+        <div className="podcastTitle text-white">
             {props.podcast_title} <br/>
             <div className="podcastDescription"> {props.podcast_description}</div>
             <div className="podcastCreator"><span class="badge badge-dark">Uploaded by {props.podcast_username}</span></div>
-  const url = "https://startechies.000webhostapp.com/end_users/banners/banner_sample_1.png";
+const url = "https://startechies.000webhostapp.com/end_users/banners/banner_sample_1.png";
     return <>
-      <div className="podcastOuter bg-dark" style={{ 
+    <div className="podcastOuter bg-dark" style={{ 
       //backgroundImage: `url("https://startechies.000webhostapp.com/end_users/banners/banner_sample_1.png")` 
-      backgroundImage: `url(${url})`
+    backgroundImage: `url(${url})`
     }}>
         <img src="https://startechies.000webhostapp.com/end_users/profile_pictures/sample_3_profile.jpg" className="profilePic img-thumbnail mx-auto d-block"/>
         <br/>
-          <div className="podcastTitle text-white">
+        <div className="podcastTitle text-white">
             TITLE <br/>
             <div className="podcastDescription">Description</div>
             <div className="podcastCreator"><span class="badge badge-dark">Uploaded by KABELO20</span></div>
 alan/v1
-          </div>
+        </div>
         <br/>
         <AudioComponent/>
         <br/> 
         <CommentComponent/>
-      </div>
+    </div>
 
-      <div class="col-md-12 text-center">
-              <button type="button" class="btn btn-primary" onClick={console.log("HERE")}>LIKES</button>
+    <div class="col-md-12 text-center">
+            <button type="button" class="btn btn-primary" onClick={console.log("HERE")}>LIKES</button>
             </div>
   </> */}
 
